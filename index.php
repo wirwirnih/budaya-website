3de52744e707b853df09bf00a26933ce38e84b03
Muhammad Wirangga <wirwirnih@gmail.com>
2021-11-13 06:08:55 +0700

    Added login form

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
-<?php echo "Hello World"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <base href="/" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.9.2/dist/css/uikit.min.css" />
+</head>
+<body>
+    <div class="uk-section uk-container">
+        <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+            <form action="#" class="uk-form-stacked js-login">
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-stacked-text">Username</label>
+                    <div class="uk-form-controls">
+                        <input type="text" required id="form-stacked-text" placeholder="Username" class="uk-input">
+                    </div>
+                </div>
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-stacked-text">Password</label>
+                    <div class="uk-form-controls">
+                        <input type="Password" required id="form-stacked-text" placeholder="Password" class="uk-input">
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <button class="uk-button uk-button-default" type="submit">Login</button>
+                </div>
+                
+            </form>
+        </div>
+    </div>
+
+    <!-- jQuery is required -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.9.2/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.9.2/dist/js/uikit-icons.min.js"></script>
+    
+</body>
+</html>
